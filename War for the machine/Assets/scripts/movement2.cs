e058d7362b74788c43e7296215948050bb2666d1
PauloGomes85848 <a85849@alunos.uminho.pt>
2021-05-13 15:32:50 +0100

    importante

---
+++ War for the machine/Assets/scripts/movement2.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour
+{
+    public float moveSpeed;
+    public float horizontal;
+    public float vertical;
+    Vector2 position;
+
+    void Update()
+    {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + moveSpeed * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+   
+}
