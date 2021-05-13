8d4db063448868c9949093f9e0511b7b5cf496b0
Guimaraes9 <a85935@alunos.uminho.pt>
2021-05-13 06:14:24 +0100

    i tried

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float horizontal;
+    public float vertical;
+    Vector2 position;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+    }
+
+    void FixedUpdate() 
+    {
+        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.5f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.5f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
